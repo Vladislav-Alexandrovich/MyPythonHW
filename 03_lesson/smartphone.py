df6491b5ccdbe9533eba6e495b4185882ff42ae0
Vladislav-Alexandrovich <vladislav.shakhov@gmail.com>
2025-09-19 14:00:44 +0300

    lesson2

---
+++ 03_lesson/smartphone.py
@@ -0,0 +1,14 @@
+class Smartphone:
+    def __init__(self, brand, model, number):
+        self.brand = brand
+        self.model = model
+        self.number = number
+
+    # def GetBrand(self):
+    #     return(self.brand)
+
+    # def GetModel(self):
+    #     return(self.model)
+    
+    # def GetNumber(self):
+    #     return(self.number)
