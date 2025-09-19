d0e2255826fbb0453b6f93803d65a19ab30c023d
Vladislav-Alexandrovich <vladislav.shakhov@gmail.com>
2025-09-19 11:44:52 +0300

    lesson1

---
+++ 01_lesson/lesson_1_task_5.py
@@ -0,0 +1,22 @@
+def print_number(num):
+    print(num, end='')
+
+print_number(8)
+print_number(8)
+print_number(0)
+print_number(0)
+print_number(5)
+print_number(5)
+print_number(5)
+print_number(3)
+print_number(5)
+print_number(3)
+print_number(5)
+
+
+
+# def print_number(_):
+#     print(_)
+#
+# for _ in range(11): print_number(88005553535)
+
