cf263a4661106b6a0f8ca20fd8858ef3306f5e67
Vladislav-Alexandrovich <vladislav.shakhov@gmail.com>
2025-09-19 16:53:02 +0300

    lesson3

---
+++ 03_lesson/lesson_3_task_1.py
@@ -0,0 +1,11 @@
+from user import User
+
+my_user = User("Сергей", "Иванов")
+
+print(my_user.First_name)
+print(my_user.Last_name)
+print(my_user.First_name, my_user.Last_name)
+
+# print (my_user.GetFirst_Name())
+# print (my_user.GetLast_Name())
+# print (my_user.GetFull_name())
---
+++ 03_lesson/mailing.py
@@ -0,0 +1,14 @@
+from address import Address
+
+
+class Mailing:
+
+    def __init__(self, from_address, to_address, cost, track):
+        self.From_address = from_address
+        self.To_address = to_address
+        self.Cost = cost
+        self.Track = track
+
+    def __str__(self):
+        return f"Отправление {self.Track} из {self.From_address}" \
+               f" в {self.To_address}. Cтоимость {self.Cost} рублей."
