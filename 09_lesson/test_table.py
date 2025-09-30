8b3fd9c2012ea2af4144e3e3b2572fcfe5f5df2a
Vladislav-Alexandrovich <vladislav.shakhov@gmail.com>
2025-09-30 17:29:22 +0300

    lesson1

---
+++ 09_lesson/test_table.py
@@ -0,0 +1,41 @@
+from sqlalchemy import create_engine, inspect, text
+
+db_connection_string = "postgresql://postgres:Study2024@localhost:5432/mybase"
+db = create_engine(db_connection_string)
+
+
+def test_db_connection():
+    inspector = inspect(db)
+    names = inspector.get_table_names()
+    assert names[1] == 'subject_id'
+
+def test_insert():
+    connection = db.connect()
+    transaction = connection.begin()
+    sql = text("INSERT INTO subject(\"subject_id\") VALUES (:new_id)")
+    connection.execute(sql, {"new_id": "17"})
+    sql = text("INSERT INTO subject(\"subject_title\") VALUES (:new_title)")
+    connection.execute(sql, {"new_title": "Art"})
+
+    transaction.commit()
+    connection.close()
+
+def test_update():
+    connection = db.connect()
+    transaction = connection.begin()
+
+    sql = text("Update subject SET subject_title = :subject WHERE subject_id = :17")
+    connection.execute(sql, {"subject": 'Painting', "id": 17})
+
+    transaction.commit()
+    connection.close()
+
+def test_delete():
+    connection = db.connect()
+    transaction = connection.begin()
+
+    sql = text("DELETE FROM subject WHERE id = :id")
+    connection.execute(sql, {"id": 17})
+
+    transaction.commit()
+    connection.close()
