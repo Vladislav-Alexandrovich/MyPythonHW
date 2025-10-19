64c6bedbd58a7573063f6d926de0d1ae3ba97349
Vladislav-Alexandrovich <vladislav.shakhov@gmail.com>
2025-10-19 11:49:45 +0300

    lesson 10 allure

--- 07_lesson/shop/shop_pages/order_page.py
+++ 07_lesson/shop/shop_pages/order_page.py
@@ -1,7 +1,4 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 class OrderPage:
